21a8339e56f1d183fa67436eaf6531fdc7e17707
Rahul <rahul.yadav@valuepitch.com>
2024-05-08 17:05:23 +0530

    Added Counter

--- static/js/app.js
+++ static/js/app.js
@@ -12,3 +12,43 @@ const copyright = () => {
   tag.textContent = year;
 };
 copyright();
+
+// Counter
+const counter = (selector) => {
+  var counted = 0;
+  $(window).scroll(function () {
+    var oTop = $(selector).offset().top - window.innerHeight;
+    if (counted == 0 && $(window).scrollTop() > oTop) {
+      $(".count").each(function () {
+        var $this = $(this),
+          countTo = $this.attr("data-count");
+        $({
+          countNum: $this.text(),
+        }).animate(
+          {
+            countNum: countTo,
+          },
+          {
+            duration: 3000,
+            easing: "swing",
+            step: function () {
+              var formattedNum = Math.floor(this.countNum);
+              if (formattedNum < 10) {
+                formattedNum = "0" + formattedNum;
+              }
+              $this.text(formattedNum);
+            },
+            complete: function () {
+              var finalNum = Math.floor(this.countNum);
+              if (finalNum < 10) {
+                finalNum = "0" + finalNum;
+              }
+              $this.text(finalNum);
+            },
+          }
+        );
+      });
+      counted = 1;
+    }
+  });
+};
