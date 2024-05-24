72f29e07d16c3d63de579d6d1275174c00b18e3f
Rahul <rahul.yadav@valuepitch.com>
2024-05-24 15:29:01 +0530

    Added New Project Design

---
+++ static/js/projects.js
@@ -0,0 +1,101 @@
+const projectList = [
+  {
+    id: 1,
+    filter: "development",
+    name: "Development Services",
+    title: "B2Btesters",
+    heading: "Web Development",
+    img: "./static/images/projects/dev/b2b.jpg",
+    link: "https://b2btesters.com/",
+  },
+  {
+    id: 2,
+    filter: "advertising",
+    name: "Advertising",
+    title: "CrimeScan",
+    heading: "Advertising Services",
+    img: "./static/images/projects/adv/crimescan.jpg",
+    link: "https://crimescan.ai/",
+  },
+];
+
+// Filter List
+const container = document.querySelector(".project-page .grid");
+const filters = document.querySelector(".project-page .filter");
+
+// Load Items
+window.addEventListener("DOMContentLoaded", () => {
+  displayItem(projectList);
+  displayBtns();
+});
+
+// Item Element
+const displayItem = (menuItems) => {
+  let items = menuItems.map((item) => {
+    return `
+    <div class="col-lg-4">
+        <a href="${item.link}" target="_blank">
+            <div class="project-card">
+                <div class="text">
+                    <h6>${item.title}</h6>
+                    <h5>${item.heading}</h5>
+                </div>
+                <div class="image">
+                    <img src="${item.img}" alt="${item.name}" />
+                </div>
+            </div>
+        </a>
+    </div>
+      `;
+  });
+  items = items.join("");
+  container.innerHTML = items;
+};
+
+const displayBtns = () => {
+  const categories = projectList.reduce(
+    (values, item) => {
+      if (!values.includes(item.filter)) {
+        values.push(item.filter);
+      }
+      return values;
+    },
+    ["All"]
+  );
+  const categorieBtns = categories
+    .map((category) => {
+      const categoryName =
+        category === "All"
+          ? "All"
+          : projectList.find((item) => item.filter === category).name;
+      const activeClass = category === "All" ? "active" : "";
+      return `
+      <button class="filter-btn ${activeClass}" data-filter="${category}">
+          ${categoryName}
+      </button>
+  `;
+    })
+    .join("");
+  filters.innerHTML = categorieBtns;
+  const filterBtns = document.querySelectorAll(".filter-btn");
+
+  // Filter Item
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      filterBtns.forEach((btn) => btn.classList.remove("active"));
+      e.currentTarget.classList.add("active");
+
+      const filter = e.currentTarget.dataset.filter;
+      const menuFilter = projectList.filter((menuItem) => {
+        if (menuItem.filter == filter) {
+          return menuItem;
+        }
+      });
+      if (filter == "All") {
+        displayItem(projectList);
+      } else {
+        displayItem(menuFilter);
+      }
+    });
+  });
+};
